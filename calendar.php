3bf1c8f686245f32835c42b1d5b77ae74b9ce93a
pansar <pansar@localhost.localdomain>
2014-05-07 08:10:05 +0200

    First commit

---
+++ calendar.php
@@ -0,0 +1,3 @@
+<?php
+	$url = 'https://www.google.com/calendar/ical/7rjegvni309hlhoph1buq52q8k%40group.calendar.google.com/private-2153851d6964f722491a6a8f48301a7f/basic.ics';
+	echo file_get_contents($url);
